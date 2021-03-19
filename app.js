5459423f58798f54f8171856adb3b64edcde722e
Italo Alves <italo.alves.jp@gmail.com>
2021-03-19 03:38:49 -0300

    Adds 404 error  handling

--- app.js
+++ app.js
@@ -11,10 +11,12 @@ app.use(morgan("dev"));
 app.use("/products", productsRoute);
 app.use("/orders", ordersRoute);
 
-app.use((req, res) => {
-  res.status(200).send({
-    mesage: "Ok",
-  });
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+
+  error.status(404);
+
+  next(erro);
 });
 
 export default app;
