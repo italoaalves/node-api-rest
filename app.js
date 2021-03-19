90af710da826aa1cbde3b9918c0ba5370c1f1e93
Italo Alves <italo.alves.jp@gmail.com>
2021-03-19 04:03:23 -0300

    Refactors the initial setup

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
 import express from "express";
+import bodyParser from "body-parser";
 import morgan from "morgan";
 
-import productsRoute from "./routes/products.js";
-import ordersRoute from "./routes/orders.js";
+import productsRoutes from "./routes/products.js";
 
 const app = express();
 
+// Middleware
 app.use(morgan("dev"));
+app.use(bodyParser.json());
 
-app.use("/products", productsRoute);
-app.use("/orders", ordersRoute);
+// Routes
+app.use("/products", productsRoutes);
 
+// Error handling
 app.use((req, res, next) => {
   const error = new Error("Not found");
-
   error.status(404);
+  next(error);
+});
 
-  next(erro);
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  return res.send({
+    error: {
+      mesage: error.mesage,
+    },
+  });
 });
 
 export default app;
