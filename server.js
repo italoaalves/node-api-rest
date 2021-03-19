8597080fb9522fbc79b562c0c97405939863b66b
Italo Alves <italo.alves.jp@gmail.com>
2021-03-19 03:36:25 -0300

    Adds orders endpoint

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-import { createServer } from 'http'
-import app from './app.js'
+import { createServer } from "http";
+import app from "./app.js";
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-const server = createServer(app)
-server.listen(port)
+const server = createServer(app);
+server.listen(port);
